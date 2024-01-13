b643abecd229683487b0da06a7d6ea5ffcb0eb87
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-01-13 23:11:49 +0200

    testing

--- sum.test.js
+++ sum.test.js
@@ -1,2 +1,11 @@
 const {sum} = require('./sum');
 
+test("Sum", () => {
+  expect(sum(4, 6)).toBe(10);
+});
+
+test("To Equal", () => {
+  let data = { name: "Daniel", age: 35 };
+  data['profession'] = "Software Engineer";
+  expect(data).toEqual({ name: "Daniel", age: 35, profession: "Software Engineer" });
+});
