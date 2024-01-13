1c4a23b7c19ee4b90b4719e2968f5316c7cf8614
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2024-01-13 23:09:24 +0200

    restarting tests

--- sum.test.js
+++ sum.test.js
@@ -1,24 +1,2 @@
 const {sum} = require('./sum');
 
-test("testing sum", () => {
-  expect(sum(4, 6)).toBe(10);
-});
-
-test("test", () => {
-  expect(sum(1, 3)).toBe(4)
-});
-
-test("two plus two is four", () => {
-  expect(2 + 2).toBe(4);
-})
-
-test("equal this object", () => {
-  const data = { name: 'Daniel', age: 35 };
-  data['profession'] = 'Software Engineer'
-  expect(data).toEqual({name: 'Daniel', age: 35, profession: 'Software Engineer'})
-})
-
-test("to be falsy", () => {
-  const n = null;
-  expect(n).toBeFalsy()
-})
